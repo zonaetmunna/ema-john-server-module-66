3c5c35380624f9a1711882f83950ef4813003ac9
zonaetmunna <zonaetmonna@gmail.com>
2021-10-31 23:28:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// database
+// username: mongodbUser
+// pass : PSTtRIeJBin49i5S
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ugo5b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+     try {
+          await client.connect();
+          const database = client.db('onlineShop');
+          const productCollection = database.collection('products');
+          const orderCollection = database.collection('order');
+
+          // GET api
+          app.get('/products', async (req, res) => {
+               console.log(req.query);
+               const cursor = productCollection.find({});
+               const page = req.query.page;
+               const size = parseInt(req.query.size);
+               let products;
+               const count = await cursor.count();
+               if (page) {
+                    products = await cursor.skip(page * size).limit(size).toArray();
+               }
+               else {
+                    products = await cursor.toArray();
+               }
+
+
+               res.send({
+                    count,
+                    products
+               });
+          })
+
+          // use post to get data by keys
+          app.post('/products/byKeys', async (req, res) => {
+               const keys = req.body;
+               const query = { key: { $in: keys } }
+               const products = await productCollection.find(query).toArray();
+               res.json(products);
+          })
+
+          // order api
+          app.post('/orders', async (req, res) => {
+               const order = req.body;
+               const result = await orderCollection.insertOne(order);
+               res.json(result);
+          }
+     }
+     finally {
+          // await client.close();
+     }
+
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+     res.send('server connected');
+})
+
+app.listen(port, () => {
+     console.log('listing the port', port);
+})
